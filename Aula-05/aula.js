943e280d2a268d947955fae6b3e24ec26ab58934
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-08-27 23:00:19 -0300

    Commit para a aula 5

--- Aula-05/aula.js
+++ Aula-05/aula.js
@@ -1,4 +1,4 @@
-function escrevaMeuNome(nome){
+/* function escrevaMeuNome(nome){
     console.log('Meu nome é ' + nome)
 }
 
@@ -12,4 +12,25 @@ function verificaIdade(idade){
     }
 }
 
-verificaIdade(48)
+verificaIdade(48) */
+
+function aplicarDesconto( principal, desconto){
+    return principal - (principal * desconto/ 100)
+}
+
+function aplicarAcrescimo ( principal, acrescimo){
+    return principal + (principal * acrescimo / 100)
+}
+
+const preco = 100.0
+const formaPagamento = 4
+
+if (formaPagamento === 1) {
+    console.log(`Preço a ser pago com 10% de desconto R$ ${(aplicarDesconto(preco, 10)).toFixed(2)}`)   
+} else if (formaPagamento === 2){
+    console.log(`Preço a ser pago com 15% de desconto R$ ${(aplicarDesconto(preco, 15)).toFixed(2)}`)   
+} else if (formaPagamento === 3) {
+    console.log(`Preço normal  R$ ${(aplicarDesconto(preco, 0)).toFixed(2)}`)      
+} else {
+    console.log(`Preço com acréscimo de 10% R$ ${(aplicarAcrescimo (preco, 10)).toFixed(2)}`)     
+}
