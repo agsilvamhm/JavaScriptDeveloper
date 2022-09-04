c3ffea5f999186cf8ba06df07f734ced4174305e
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-09-04 15:06:03 -0300

    Commit do projeto

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,4 +1,30 @@
-const btnTeste = document.getElementById("btnTeste")
-btnTeste.addEventListener('click', function(){
-    alert ('Clicou no botão!')
-})
+function convertePokemonTypesToLi(pokemonTypes){
+    return pokemonTypes.map((typeSlots) => {
+        return `
+        <li class="type">${typeSlots.type.name}</li>
+        `
+    })
+}
+
+function convertePokemonToLi(pokemon){
+    return `
+    <li class="pokemon">
+        <span class="number">${pokemon.order}</span>
+        <span class="name">${pokemon.name}</span>
+    
+        <div class="detail">
+        <ol class="types">
+           ${convertePokemonTypesToLi(pokemon.types).join('')}
+        </ol>
+        <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+        </div>
+    </li>
+    `
+ }   
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML = pokemons.map(convertePokemonToLi).join('')
+    })
+   
