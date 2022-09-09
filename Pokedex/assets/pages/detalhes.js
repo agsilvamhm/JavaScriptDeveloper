7da932dd7279d9773a40f74f3d9bed08112ba185
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-09-08 23:06:18 -0300

    Pokedex funcionando

--- Pokedex/assets/pages/detalhes.js
+++ Pokedex/assets/pages/detalhes.js
@@ -1,1 +1,23 @@
-console.log('cheguei')
+let urlForm = 'https://pokeapi.co/api/v2/pokemon/'
+
+const urlParam = new URLSearchParams(window.location.search);
+let idPokemon = urlParam.get("id");
+
+urlForm = urlForm + idPokemon
+
+fetch(urlForm)
+   .then(resposta => resposta.json())
+   .then(function(dados){
+        console.log(dados.name)
+        console.log(dados.types[0].type.name)
+ 
+        let imagemFrente = document.getElementById("imagemFrente")
+     
+        imagemFrente.src = dados.sprites.other.dream_world.front_default
+ 
+   })
+   .catch(function(err){
+        console.log(err)
+   })
+
+ 
